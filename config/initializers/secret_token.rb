6704cabdbf7c1bb2ebdc9b87f6728ecd65a98ced
pdostal <git@pdostal.cz>
2014-08-05 12:21:25 +0200

    Create RefineryCMS project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stomatologieujezd::Application.config.secret_token = 'c12c7aa9b4c7b1128b04a76aa9bec6f72da27a1a9736e338809cd88015b4e7819d7d08f63be8aa3dbf79fca72a15d641e684bffa5ae150de64032b32538396ed'
